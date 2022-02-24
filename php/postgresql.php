d5021d3e6ba5306c38525c1644fa3e9473b94ee5
Vyasdev <65308856+Vyasdev217@users.noreply.github.com>
2022-02-24 18:00:22 +0530

    Create postgresql.php

---
+++ php/postgresql.php
@@ -0,0 +1,52 @@
+<?php
+# This function reads your DATABASE_URL config var and returns a connection (Used in heroku)
+function pg_connection_string_from_database_url() {
+  extract(parse_url($_ENV["DATABASE_URL"]));
+  return "user=$user password=$pass host=$host dbname=" . substr($path, 1);
+}
+
+# Make a db query
+function db_query($q){
+  # Establish the connection
+  $pg_conn = pg_connect(pg_connection_string_from_database_url());
+  # Make the query and get response
+  $response = pg_query($pg_conn,$q);
+  # Close the connection
+  pg_close();
+  $result=[];# To store result rows
+  while ($data = pg_fetch_object($response))
+    array_push($result,$data);
+  return $result;
+}
+
+# SQL INSERT
+function db_insert($table,$data){
+  $data_keys=array_keys($data);
+  $keys="";
+  $values="";
+  for($i=0;$i<count($data_keys);$i++){
+    $keys=$keys.",".$data_keys[i];
+    $values=$values.","."'".$data[$data_keys[i]]."'";
+  }
+  # To remove comma at beginning of string
+  $keys=substr($keys,1);
+  $values=substr($values,1);
+  db_query("INSERT INTO ".$table." ("+$keys+") VALUES (".$values.");");
+}
+
+# SQL DELETE
+function db_delete($table,$condition){
+  $data_keys=array_keys($data);
+  for($i=0;$i<count($data_keys);$i++)
+    $condition_str=$condition_str.",".$data_keys[i]."="."'".$data[$data_keys[i]]."'";
+  $condition_str=substr($condition_str,1);
+  db_query("DELETE FROM ".$table."WHERE ".$condition_str.";");
+}
+
+// TODO: Complete db_edit()
+# SQL UPDATE
+function db_edit($table,$data){
+  db_query("UPDATE ".$table." SET ".$update_str." WHERE ".$update_condition_str);
+}
+
+?>
